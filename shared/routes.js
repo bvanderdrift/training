d11b5b9d1eb91799e9ef0056ae8959a08d8c92fb
Beer van der Drift <beerdrift@gmail.com>
2015-11-21 12:14:12 +0000

    added a learner page. Placeholder content.

--- shared/routes.js
+++ shared/routes.js
@@ -11,3 +11,16 @@ Router.route('/browse', {
 	name: 'browse',
 	template: 'browse'
 });
+
+Router.route('/lesson/:_id', {
+	name: 'lesson',
+	template: 'lesson',
+	data: function(){
+		return lessons.findOne({_id: this.params._id});
+	}
+});
+
+Router.route('create/learner', {
+	name: 'createLearner',
+	template: 'createLearner'
+})
