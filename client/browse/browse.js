cc5ea33371fefda9ff55fbad4fdab49f1f676e78
Beer van der Drift <beerdrift@gmail.com>
2015-11-21 10:21:20 +0000

    Added responsive searching

--- client/browse/browse.js
+++ client/browse/browse.js
@@ -1,5 +1,23 @@
+Session.set("filter", {});
+
 Template.browse.helpers({
 	lessons: function(){
-		return lessons.find({});
+		var regex = new RegExp(".*" + Session.get("searchInput") + ".*", "i");
+		return lessons.find({
+			$or: [
+				{title: regex},
+				{description: regex},
+				{category: regex},
+				{teacher: regex},
+			]
+		});
+	}
+});
+
+Template.browse.events({
+	'keyup #searchField': function(e){
+		e.preventDefault();
+
+		Session.set("searchInput", $("#searchField").val());
 	}
 });
--- shared/collections.js
+++ shared/collections.js
@@ -3,18 +3,34 @@ lessons = new Meteor.Collection('lessons');
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if(lessons.find({}).count() == 0){
-		lessons.insert(mockupLessons());
+		mockupLessons();
 	}
   });
+
+  Meteor.methods({
+  	removeLessons: function(){
+  		lessons.remove({});
+  	}
+  })
 }
 
 //Return a single mockup lesson right now for development purposes
 function mockupLessons(){
+	lessons.insert(mockupLesson("Superawesome French Lessons",
+		"languages/french",
+		"Hi, I'm really into French and would like to learn you new stuff",
+		15,
+		"Madame French"));
+	lessons.insert(mockupLesson("Supercool Singing Lessons", "music/singing", "Ladiela", 10, "Isabelle"));
+	lessons.insert(mockupLesson("Painting Is Cool", "art/paining", "Learn to draw beautiful little trees", 26, "Bob Ross"));
+}
+
+function mockupLesson(title, category, description, hourlyPrice, teacher){
 	return {
-		title: "Superawesome French Lessons",
-		category: "languages/french",
-		description: "Hi, I'm really into French and would like to learn you new stuff",
-		hourlyPrice: 15,
-		teacher: "Madame French"
-	}
+		title: title,
+		category: category,
+		description: description,
+		hourlyPrice: hourlyPrice,
+		teacher: teacher
+	};
 }
