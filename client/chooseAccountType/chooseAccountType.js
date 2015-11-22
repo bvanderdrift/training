92b490fe00338eb2bf7db05f75dff60829ad9596
Beer van der Drift <beerdrift@gmail.com>
2015-11-22 03:07:37 +0000

    Did the layout of the choose your account type.

--- client/chooseAccountType/chooseAccountType.js
+++ client/chooseAccountType/chooseAccountType.js
@@ -1,3 +1,7 @@
+Template.chooseAccountType.onRendered(function(){
+	InitializeCircles();
+});
+
 Template.chooseAccountType.events({
 	"click #teacherBTN": function(e){
 		e.preventDefault();
@@ -13,4 +17,30 @@ Template.chooseAccountType.events({
 
 function SetAccountType(type){
 	Meteor.users.update(Meteor.userId(), {$set: {"profile.accountType": type}});
+}
+
+function InitializeCircles(){
+	var c1 = document.getElementById("circleCanvasTeacher");
+	var c2 = document.getElementById("circleCanvasLearner");
+
+	c1.width = 500;
+	c1.height = c1.width;
+
+	c2.width = 500
+	c2.height = c2.width;
+
+	DrawCircle(c1);
+	DrawCircle(c2);
+}
+
+function DrawCircle(canvas){
+	var context = canvas.getContext('2d');
+	var centerX = canvas.width / 2;
+	var centerY = canvas.height / 2;
+	var radius = .9 * Math.min(centerX, centerY);
+
+	context.beginPath();
+	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+	context.fillStyle = '#e64c2e';
+	context.fill();
 }
